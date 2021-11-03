6c020f7f1c0f8cdaa28760b00d50b635c029115b
Benjamin Dunton <benjamin.dunton@gmail.com>
2021-11-03 16:36:18 -0400

    Fixed formatting for PEP 8 compliance

--- BackingGrid.py
+++ BackingGrid.py
@@ -1,138 +1,140 @@
 from Coordinate import Coordinate
 from functools import reduce
 
+
 # TODO:
 # There is a recurring pattern of iterating through the cells in a piece
 # and tracking the grid coordinates along the way (see add() and is_collision()). I could write a generator that takes
 # a piece and for each iteration gives me a tuple (grid_x, grid_y, filled?)
 
-        
+
 # flatten the grid into a list.
 def _grid_as_list(grid):
-  l = []
-  for row in grid:
-    l.extend([cell for cell in row])
-  return l
+    l = []
+    for row in grid:
+        l.extend([cell for cell in row])
+    return l
+
 
 # Chop off num_rows rows from the top of the given Piece bitmap
 def _clip_from_top(bitmap, num_rows):
-  return bitmap[num_rows:]
- 
+    return bitmap[num_rows:]
+
 
 # Represents all the pieces that will never move again.
 # This class flattens the pieces to a 2D array for easily
 # checking whether pieces have collided
 class BackingGrid(object):
-  def __init__(self, width, height):
-    self.width = width
-    self.height = height
-    self.grid = []
-       
-    # The tuple is (filled?, y-grid (row), x-grid (col), color)
-    # It might seem redundant to store the x,y in the tuple when the
-    # position in self.grid is the same thing. It's so the renderer,
-    # who only has access to the tuple, can see the grid-relative location
-    self.grid = [[(False,None,None,None) for col in range(width)] for row in range(height)]
-  
-  def __iter__(self):
-    return iter(_grid_as_list(self.grid))
-    
-  def add(self, piece):
-    # for bounds checking, coords are based on playing field
-    x = piece.getX(Coordinate.PLAYING_FIELD)
-    y = piece.getY(Coordinate.PLAYING_FIELD)
-    self.verify_in_bounds(x, y, piece)
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = []
 
-    # Get the conversion offset for grid coords to playing field coords
-    # Add this to field coords to get grid coords
-    x_offset = piece.getX(Coordinate.GRID) - x
-    y_offset = piece.getY(Coordinate.GRID) - y
-    
-    # break apart the piece into individual cells
-    mask = piece.get_fill_mask()
-    x_field = x
-    y_field = y
-    for row in mask:
-      for cell in row:
-        if cell == 1:
-          if (self.grid[y_field][x_field][0] == True):
-            raise Exception('Tried to add a piece to a cell that was already occupied')
-          self.grid[y_field][x_field] = (True, 
-              x_field + x_offset, y_field + y_offset, piece.get_color())
-        x_field += 1
-      x_field = x
-      y_field += 1
+        # The tuple is (filled?, y-grid (row), x-grid (col), color)
+        # It might seem redundant to store the x,y in the tuple when the
+        # position in self.grid is the same thing. It's so the renderer,
+        # who only has access to the tuple, can see the grid-relative location
+        self.grid = [[(False, None, None, None) for col in range(width)] for row in range(height)]
 
-  # Throws an exception if this piece does not fit in the play area
-  # target_x: the x-coordinate in the playing field to test
-  # target_y: the y-coordinate in the playing field to test
-  # piece: the piece to consider in the (target_x, target_y) position
-  def verify_in_bounds(self, target_x, target_y, piece):
-    width_oob = target_x < 0 or target_x + piece.get_width() > self.width
-    height_oob = target_y < 0 or target_y + piece.get_height() > self.height
-    if width_oob or height_oob:
-      raise Exception('Piece out of bounds')
+    def __iter__(self):
+        return iter(_grid_as_list(self.grid))
 
-  # checks whether a piece (represented by a bitmap) collides
-  # with any other pieces in the board.
-  # Assumptions: x must be in bounds. If y is negative, only the portion of the
-  #   piece in bounds will be considered.
-  # x and y are relative to the playing field
-  def is_collision(self, x, y, bitmap):
-    # If the piece is above the top of the play area (e.g. rotating the Bar piece)
-    # only check the rows that are in bounds. Have to do this because the backing
-    # grid does not have data for negative rows.
-    if y < 0:
-      bitmap = _clip_from_top(bitmap, 0-y)
-      y = 0
-      
-    # it is a collision if any filled cell overlaps a filled
-    # cell already in the grid
-    backing_x = x
-    backing_y = y
-    for row in bitmap:
-      backing_x = x
-      for bit in row: 
-        if self.grid[backing_y][backing_x][0] == True and bit == 1:
-          return True
-        backing_x += 1
-      backing_y += 1
-    return False
+    def add(self, piece):
+        # for bounds checking, coords are based on playing field
+        x = piece.getX(Coordinate.PLAYING_FIELD)
+        y = piece.getY(Coordinate.PLAYING_FIELD)
+        self.verify_in_bounds(x, y, piece)
 
-  # Clears any rows that are filled all the way across. Collapses everything above into those newly empty rows
-  # Returns the number of rows that were cleared
-  def clear_filled_rows(self):
-    # full_indication[i] is True if grid[i] is full 
-    full_indication = list(map(
-        lambda row: reduce(lambda prev, curr: prev == curr[0] == True, row, True),
-        self.grid))
-        
-    # convert that indicator list into a list of indices
-    full_rows = []
-    for i in range(0, self.height):
-      if full_indication[i]:
-        full_rows.append(i)
+        # Get the conversion offset for grid coords to playing field coords
+        # Add this to field coords to get grid coords
+        x_offset = piece.getX(Coordinate.GRID) - x
+        y_offset = piece.getY(Coordinate.GRID) - y
 
-    if not full_rows:
-      return 0
-    
-    # shift the coordinates in the cell tuples by y_offset
-    def fudge_coords(row, y_offset):
-      for pos, cell in enumerate(row):
-        if cell[0]:
-          row[pos] = (cell[0], cell[1], cell[2] + y_offset, cell[3])
-          
-    # Collapse from bottom to top (top to bottom would require multiple passes if more than one row was cleared)
-    dest = full_rows[-1]
-    src = dest - 1
-    while src >= 0:
-      while src in full_rows:
-        src -= 1
-      self.grid[dest] = self.grid[src]
-      fudge_coords(self.grid[dest], dest-src)
-      src -= 1
-      dest -= 1
-    for y in range(len(full_rows)):
-      self.grid[y] = [(False,None,None,None)]*self.width
-    
-    return len(full_rows)
+        # break apart the piece into individual cells
+        mask = piece.get_fill_mask()
+        x_field = x
+        y_field = y
+        for row in mask:
+            for cell in row:
+                if cell == 1:
+                    if (self.grid[y_field][x_field][0] == True):
+                        raise Exception('Tried to add a piece to a cell that was already occupied')
+                    self.grid[y_field][x_field] = (True,
+                                                   x_field + x_offset, y_field + y_offset, piece.get_color())
+                x_field += 1
+            x_field = x
+            y_field += 1
+
+    # Throws an exception if this piece does not fit in the play area
+    # target_x: the x-coordinate in the playing field to test
+    # target_y: the y-coordinate in the playing field to test
+    # piece: the piece to consider in the (target_x, target_y) position
+    def verify_in_bounds(self, target_x, target_y, piece):
+        width_oob = target_x < 0 or target_x + piece.get_width() > self.width
+        height_oob = target_y < 0 or target_y + piece.get_height() > self.height
+        if width_oob or height_oob:
+            raise Exception('Piece out of bounds')
+
+    # checks whether a piece (represented by a bitmap) collides
+    # with any other pieces in the board.
+    # Assumptions: x must be in bounds. If y is negative, only the portion of the
+    #   piece in bounds will be considered.
+    # x and y are relative to the playing field
+    def is_collision(self, x, y, bitmap):
+        # If the piece is above the top of the play area (e.g. rotating the Bar piece)
+        # only check the rows that are in bounds. Have to do this because the backing
+        # grid does not have data for negative rows.
+        if y < 0:
+            bitmap = _clip_from_top(bitmap, 0 - y)
+            y = 0
+
+        # it is a collision if any filled cell overlaps a filled
+        # cell already in the grid
+        backing_x = x
+        backing_y = y
+        for row in bitmap:
+            backing_x = x
+            for bit in row:
+                if self.grid[backing_y][backing_x][0] == True and bit == 1:
+                    return True
+                backing_x += 1
+            backing_y += 1
+        return False
+
+    # Clears any rows that are filled all the way across. Collapses everything above into those newly empty rows
+    # Returns the number of rows that were cleared
+    def clear_filled_rows(self):
+        # full_indication[i] is True if grid[i] is full 
+        full_indication = list(map(
+            lambda row: reduce(lambda prev, curr: prev == curr[0] == True, row, True),
+            self.grid))
+   
+        # convert that indicator list into a list of indices
+        full_rows = []
+        for i in range(0, self.height):
+            if full_indication[i]:
+                full_rows.append(i)
+
+        if not full_rows:
+            return 0
+
+        # shift the coordinates in the cell tuples by y_offset
+        def fudge_coords(row, y_offset):
+            for pos, cell in enumerate(row):
+                if cell[0]:
+                    row[pos] = (cell[0], cell[1], cell[2] + y_offset, cell[3])
+
+        # Collapse from bottom to top (top to bottom would require multiple passes if more than one row was cleared)
+        dest = full_rows[-1]
+        src = dest - 1
+        while src >= 0:
+            while src in full_rows:
+                src -= 1
+            self.grid[dest] = self.grid[src]
+            fudge_coords(self.grid[dest], dest - src)
+            src -= 1
+            dest -= 1
+        for y in range(len(full_rows)):
+            self.grid[y] = [(False, None, None, None)] * self.width
+
+        return len(full_rows)
--- test/TestBackingGrid.py
+++ test/TestBackingGrid.py
@@ -1,6 +1,7 @@
 import unittest
 
 import sys, os
+
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from BackingGrid import BackingGrid
 from Geometry import Geometry
@@ -8,232 +9,232 @@ from Bar import Bar
 from Box import Box
 from Tee import Tee
 
+
 # TODO: the code would be more testable if I could mock the Piece class and create pieces of arbitrary size
 class TestBackingGrid(unittest.TestCase):
-  
-  WIDTH = 10
-  HEIGHT = 10
+    WIDTH = 10
+    HEIGHT = 10
 
-  def setUp(self):
-    self.grid = BackingGrid(TestBackingGrid.WIDTH, TestBackingGrid.HEIGHT)
-    self.geo = Geometry(20, 20, (0,0,100,100))
+    def setUp(self):
+        self.grid = BackingGrid(TestBackingGrid.WIDTH, TestBackingGrid.HEIGHT)
+        self.geo = Geometry(20, 20, (0, 0, 100, 100))
 
-  def test_ieterate_over_nonempty_grid(self):
-    box = Box(self.geo)
-    box.setX(0)
-    box.setY(0)
-    self.grid.add(box)
+    def test_ieterate_over_nonempty_grid(self):
+        box = Box(self.geo)
+        box.setX(0)
+        box.setY(0)
+        self.grid.add(box)
 
-    actual_list = list(self.grid)
-    self.assertEqual(100, len(actual_list))
+        actual_list = list(self.grid)
+        self.assertEqual(100, len(actual_list))
 
-    # verify the spaces that are filled by a block
-    self.assertEqual((True, 0, 0, 'green'), actual_list[0])
-    self.assertEqual((True, 1, 0, 'green'), actual_list[1])
-    self.assertEqual((True, 0, 1, 'green'), actual_list[10])
-    self.assertEqual((True, 1, 1, 'green'), actual_list[11])
+        # verify the spaces that are filled by a block
+        self.assertEqual((True, 0, 0, 'green'), actual_list[0])
+        self.assertEqual((True, 1, 0, 'green'), actual_list[1])
+        self.assertEqual((True, 0, 1, 'green'), actual_list[10])
+        self.assertEqual((True, 1, 1, 'green'), actual_list[11])
 
-    # verify everything else in bulk
-    filled_spaces = [0, 1, 10, 11]
-    for i in range(0, len(actual_list)):
-      if i not in filled_spaces:
-        self.assertEqual((False, None, None, None), actual_list[i])
+        # verify everything else in bulk
+        filled_spaces = [0, 1, 10, 11]
+        for i in range(0, len(actual_list)):
+            if i not in filled_spaces:
+                self.assertEqual((False, None, None, None), actual_list[i])
 
-  def test_iterate_over_empty_grid(self):
-    actual_list = list(self.grid)
-    for i in range(0, 99):
-      self.assertEqual((False, None, None, None), actual_list[i])
+    def test_iterate_over_empty_grid(self):
+        actual_list = list(self.grid)
+        for i in range(0, 99):
+            self.assertEqual((False, None, None, None), actual_list[i])
 
-  def test_box_collision(self):
-    box = Box(self.geo)
-    box.setX(0)
-    box.setY(0)
-    self.grid.add(box)
+    def test_box_collision(self):
+        box = Box(self.geo)
+        box.setX(0)
+        box.setY(0)
+        self.grid.add(box)
 
-    self.assertTrue(self.grid.is_collision(1, 1, box.get_fill_mask()))
-    self.assertTrue(self.grid.is_collision(0, 0, box.get_fill_mask()))
-    self.assertTrue(self.grid.is_collision(1, 0, box.get_fill_mask()))
-    self.assertTrue(self.grid.is_collision(0, 1, box.get_fill_mask()))
-    self.assertFalse(self.grid.is_collision(2, 0, box.get_fill_mask()))
-    
-  def test_tee_collision(self):
-    tee = Tee(self.geo)
-    self.grid.add(tee)
-    self.assertTrue(self.grid.is_collision(1, 1, tee.get_fill_mask()))
-    self.assertTrue(self.grid.is_collision(0, 1, tee.get_fill_mask()))
-    self.assertFalse(self.grid.is_collision(2,2, tee.get_fill_mask()))
+        self.assertTrue(self.grid.is_collision(1, 1, box.get_fill_mask()))
+        self.assertTrue(self.grid.is_collision(0, 0, box.get_fill_mask()))
+        self.assertTrue(self.grid.is_collision(1, 0, box.get_fill_mask()))
+        self.assertTrue(self.grid.is_collision(0, 1, box.get_fill_mask()))
+        self.assertFalse(self.grid.is_collision(2, 0, box.get_fill_mask()))
 
-  def test_add_overlapping(self):
-    box1 = Box(self.geo)
-    box1.setX(0)
-    box1.setY(0)
+    def test_tee_collision(self):
+        tee = Tee(self.geo)
+        self.grid.add(tee)
+        self.assertTrue(self.grid.is_collision(1, 1, tee.get_fill_mask()))
+        self.assertTrue(self.grid.is_collision(0, 1, tee.get_fill_mask()))
+        self.assertFalse(self.grid.is_collision(2, 2, tee.get_fill_mask()))
 
-    box2 = Box(self.geo)
-    box2.setX(1)
-    box2.setY(1)
+    def test_add_overlapping(self):
+        box1 = Box(self.geo)
+        box1.setX(0)
+        box1.setY(0)
 
-    self.grid.add(box1)
-    self.assertRaisesRegex(Exception, '^.*already occupied.*$', lambda: self.grid.add(box2))
+        box2 = Box(self.geo)
+        box2.setX(1)
+        box2.setY(1)
 
-  def test_add_adjacent(self):
-    box1 = Box(self.geo)
-    box1.setX(0)
-    box1.setY(0)
+        self.grid.add(box1)
+        self.assertRaisesRegex(Exception, '^.*already occupied.*$', lambda: self.grid.add(box2))
 
-    box2 = Box(self.geo)
-    box2.setX(2)
-    box2.setY(1)
-    self.grid.add(box1)
-    # we're just testing that this doesn't raise
-    self.grid.add(box2)
+    def test_add_adjacent(self):
+        box1 = Box(self.geo)
+        box1.setX(0)
+        box1.setY(0)
 
-  # add a piece that goes off screen
-  def test_piece_partially_out_of_bounds_right(self):
-    box = Box(self.geo)
-    box.setX(9) # this spot is in bounds, but the width of the shape pushes it out
-    box.setY(0)
-    self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
+        box2 = Box(self.geo)
+        box2.setX(2)
+        box2.setY(1)
+        self.grid.add(box1)
+        # we're just testing that this doesn't raise
+        self.grid.add(box2)
 
-  # add a piece that goes off screen
-  def test_piece_partially_out_of_bounds_bottom(self):
-    box = Box(self.geo)
-    box.setX(0)
-    box.setY(9) # this spot is in bounds, but the height of the shape pushes it out
-    self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
+    # add a piece that goes off screen
+    def test_piece_partially_out_of_bounds_right(self):
+        box = Box(self.geo)
+        box.setX(9)  # this spot is in bounds, but the width of the shape pushes it out
+        box.setY(0)
+        self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
 
-  def test_piece_partially_out_of_bounds_left(self):
-      box = Box(self.geo)
-      box.setX(-1)
-      box.setY(0)
-      self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
+    # add a piece that goes off screen
+    def test_piece_partially_out_of_bounds_bottom(self):
+        box = Box(self.geo)
+        box.setX(0)
+        box.setY(9)  # this spot is in bounds, but the height of the shape pushes it out
+        self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
 
-  def test_piece_partially_out_of_bounds_top(self):
-    box = Box(self.geo)
-    box.setX(0)
-    box.setY(-1)
-    self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
+    def test_piece_partially_out_of_bounds_left(self):
+        box = Box(self.geo)
+        box.setX(-1)
+        box.setY(0)
+        self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
 
-  def test_piece_fully_out_of_bounds_right(self):
-    box = Box(self.geo)
-    box.setX(10)
-    box.setY(0)
-    self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
+    def test_piece_partially_out_of_bounds_top(self):
+        box = Box(self.geo)
+        box.setX(0)
+        box.setY(-1)
+        self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
 
-  def test_new_piece_completes_bottom_row(self):
-      bar1 = Bar(self.geo)
-      bar1.setX(0)
-      bar1.setY(9)
+    def test_piece_fully_out_of_bounds_right(self):
+        box = Box(self.geo)
+        box.setX(10)
+        box.setY(0)
+        self.assertRaisesRegex(Exception, '^.*out of bounds*$', lambda: self.grid.add(box))
 
-      bar2 = Bar(self.geo)
-      bar2.setX(bar1.get_width())
-      bar2.setY(9)
+    def test_new_piece_completes_bottom_row(self):
+        bar1 = Bar(self.geo)
+        bar1.setX(0)
+        bar1.setY(9)
 
-      box = Box(self.geo)
-      box.setX(bar1.get_width() + bar2.get_width())
-      box.setY(8)
+        bar2 = Bar(self.geo)
+        bar2.setX(bar1.get_width())
+        bar2.setY(9)
 
-      self.grid.add(bar1)
-      self.grid.add(bar2)
-      self.assertEqual(0, self.grid.clear_filled_rows())
-      self.grid.add(box)
-      self.assertEqual(1, self.grid.clear_filled_rows())
+        box = Box(self.geo)
+        box.setX(bar1.get_width() + bar2.get_width())
+        box.setY(8)
 
-      # how can I test without access to the underlying grid?
-      self.fail("TOD: verify that everything else is shifted down")
+        self.grid.add(bar1)
+        self.grid.add(bar2)
+        self.assertEqual(0, self.grid.clear_filled_rows())
+        self.grid.add(box)
+        self.assertEqual(1, self.grid.clear_filled_rows())
 
-  def test_new_piece_completes_top_row(self):
-    bar1 = Bar(self.geo)
-    bar1.setX(0)
-    bar1.setY(0)
+        # how can I test without access to the underlying grid?
+        self.fail("TOD: verify that everything else is shifted down")
 
-    bar2 = Bar(self.geo)
-    bar2.setX(bar1.get_width())
-    bar2.setY(0)
+    def test_new_piece_completes_top_row(self):
+        bar1 = Bar(self.geo)
+        bar1.setX(0)
+        bar1.setY(0)
 
-    box = Box(self.geo)
-    box.setX(bar1.get_width() + bar2.get_width())
-    box.setY(0)
+        bar2 = Bar(self.geo)
+        bar2.setX(bar1.get_width())
+        bar2.setY(0)
 
-    self.grid.add(bar1)
-    self.grid.add(bar2)
-    # a precondition of the test is that no row is filled yet
-    self.assertEqual(0, self.grid.clear_filled_rows())
-    self.grid.add(box)
-    self.assertEqual(1, self.grid.clear_filled_rows())
+        box = Box(self.geo)
+        box.setX(bar1.get_width() + bar2.get_width())
+        box.setY(0)
 
-    self.fail("TOD: verify that everything above is shifted down, and nothing below is shifted")
+        self.grid.add(bar1)
+        self.grid.add(bar2)
+        # a precondition of the test is that no row is filled yet
+        self.assertEqual(0, self.grid.clear_filled_rows())
+        self.grid.add(box)
+        self.assertEqual(1, self.grid.clear_filled_rows())
 
-  def test_new_piece_completes_nonbottom_row(self):
-      # set up 2 horizontal bars that can be completed with a box
-      bar = Bar(self.geo)
-      bar.setX(0)
-      bar.setY(7)
-      bar2 = Bar(self.geo)
-      bar2.setX(bar.get_width())
-      bar2.setY(7)
+        self.fail("TOD: verify that everything above is shifted down, and nothing below is shifted")
 
-      # this is on the bottom, beneath the two horizontal bars
-      box = Box(self.geo)
-      box.setX(5)
-      box.setY(8)
+    def test_new_piece_completes_nonbottom_row(self):
+        # set up 2 horizontal bars that can be completed with a box
+        bar = Bar(self.geo)
+        bar.setX(0)
+        bar.setY(7)
+        bar2 = Bar(self.geo)
+        bar2.setX(bar.get_width())
+        bar2.setY(7)
 
-      # this completes row 7
-      box2 = Box(self.geo)
-      box2.setX(bar.get_width() + bar2.get_width())
-      box2.setY(7)
+        # this is on the bottom, beneath the two horizontal bars
+        box = Box(self.geo)
+        box.setX(5)
+        box.setY(8)
 
-      self.grid.add(bar)
-      self.grid.add(bar2)
-      self.grid.add(box)
-      # precondition of test: should not have completed a row yet
-      self.assertEqual(0, self.grid.clear_filled_rows())
-      self.grid.add(box2)
-      self.assertEqual(1, self.grid.clear_filled_rows())
+        # this completes row 7
+        box2 = Box(self.geo)
+        box2.setX(bar.get_width() + bar2.get_width())
+        box2.setY(7)
 
-      self.fail("TOD: verify that everything else is shifted down")
+        self.grid.add(bar)
+        self.grid.add(bar2)
+        self.grid.add(box)
+        # precondition of test: should not have completed a row yet
+        self.assertEqual(0, self.grid.clear_filled_rows())
+        self.grid.add(box2)
+        self.assertEqual(1, self.grid.clear_filled_rows())
 
-  # a piece causes two noncontiguous rows to be full
-  def test_piece_completes_noncontiguous_rows(self):
-      bottom_left_bar = Bar(self.geo)
-      bottom_left_bar.setX(0)
-      bottom_left_bar.setY(9)
+        self.fail("TOD: verify that everything else is shifted down")
 
-      bottom_right_bar = Bar(self.geo)
-      bottom_right_bar.setX(bottom_left_bar.get_width())
-      bottom_right_bar.setY(9)
+    # a piece causes two noncontiguous rows to be full
+    def test_piece_completes_noncontiguous_rows(self):
+        bottom_left_bar = Bar(self.geo)
+        bottom_left_bar.setX(0)
+        bottom_left_bar.setY(9)
 
-      top_left_bar = Bar(self.geo)
-      top_left_bar.setX(0)
-      top_left_bar.setY(7)
+        bottom_right_bar = Bar(self.geo)
+        bottom_right_bar.setX(bottom_left_bar.get_width())
+        bottom_right_bar.setY(9)
 
-      top_right_bar = Bar(self.geo)
-      top_right_bar.setX(top_left_bar.get_width())
-      top_right_bar.setY(7)
+        top_left_bar = Bar(self.geo)
+        top_left_bar.setX(0)
+        top_left_bar.setY(7)
 
-      vertical_bar = Bar(self.geo)
-      vertical_bar.rotate(1)
-      vertical_bar.setX(8)
-      vertical_bar.setY(6)
+        top_right_bar = Bar(self.geo)
+        top_right_bar.setX(top_left_bar.get_width())
+        top_right_bar.setY(7)
 
-      vertical_bar2 = Bar(self.geo)
-      vertical_bar2.rotate(1)
-      vertical_bar2.setX(9)
-      vertical_bar2.setY(6)
+        vertical_bar = Bar(self.geo)
+        vertical_bar.rotate(1)
+        vertical_bar.setX(8)
+        vertical_bar.setY(6)
 
-      self.grid.add(bottom_left_bar)
-      self.grid.add(bottom_right_bar)
-      self.grid.add(top_left_bar)
-      self.grid.add(top_right_bar)
-      self.grid.add(vertical_bar)
-      self.assertEqual(0, self.grid.clear_filled_rows())
-      self.grid.add(vertical_bar2)
-      self.assertEqual(2, self.grid.clear_filled_rows())
+        vertical_bar2 = Bar(self.geo)
+        vertical_bar2.rotate(1)
+        vertical_bar2.setX(9)
+        vertical_bar2.setY(6)
 
-      self.fail("TOD: verify that everything else is shifted down")
+        self.grid.add(bottom_left_bar)
+        self.grid.add(bottom_right_bar)
+        self.grid.add(top_left_bar)
+        self.grid.add(top_right_bar)
+        self.grid.add(vertical_bar)
+        self.assertEqual(0, self.grid.clear_filled_rows())
+        self.grid.add(vertical_bar2)
+        self.assertEqual(2, self.grid.clear_filled_rows())
 
-  def test_piece_completes_two_adjacent_rows(self):
-      self.fail("TODO: implement me")
+        self.fail("TOD: verify that everything else is shifted down")
 
-    
+    def test_piece_completes_two_adjacent_rows(self):
+        self.fail("TODO: implement me")
+
+
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
